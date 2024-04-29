eaaeff1d167548badece1ae57f52de10f59be2ed
Fayyaz <abdullahnaufal16@gmail.com>
2024-04-29 15:17:21 +0700

    first push

---
+++ carousel.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+//     $('#image-carousel').carousel({
+//         interval: 100, // Atur kecepatan autoplay di sini (dalam milidetik)
+//         ride: 'carousel',
+//         wrap: false // Bergulir terus-menerus
+//     });
+// });
