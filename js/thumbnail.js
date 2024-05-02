784433544a46ba1a0a77b1f8f8d3bc5c3786c84f
Fayyaz <abdullahnaufal16@gmail.com>
2024-05-02 16:07:42 +0700

    JS

---
+++ js/thumbnail.js
@@ -0,0 +1,9 @@
+var thumbnailsContainer = document.querySelector('.thumbnails-container');
+var thumbnails = document.querySelectorAll('.thumbnail');
+
+thumbnailsContainer.addEventListener('wheel', function(event) {
+  event.preventDefault();
+  
+  var scrollAmount = event.deltaY;
+  thumbnailsContainer.scrollLeft += scrollAmount;
+});
